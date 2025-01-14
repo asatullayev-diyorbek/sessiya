99dfabaf6caabdd37a2cd7b649a9420295e58063
asatullayev-diyorbek <asatullayevdiyorbek3@gmail.com>
2025-01-14 09:22:08 +0500

    fixed bugs

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,4 +8,4 @@ app_name = 'config'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls.static import static
-from django.urls import path, include
-
-from config import settings
+from django.urls import path
 from main import views
 
 app_name = 'main'
 urlpatterns = [
     path('', views.Dashboard.as_view(), name='dashboard'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
